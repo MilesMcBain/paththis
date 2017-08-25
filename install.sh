20a3f8da45c9f3a4a3018fba148203c474c97b84
milesmcbain <miles.mcbain@gmail.com>
2017-08-25 23:02:16 +1000

    Fix install detection of $HOME/bin in path

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@ echo "copying paththis to ~/bin/paththis"
 CLONE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 mkdir -p ~/bin
 cp $CLONE_DIR/paththis ~/bin/
-if [[ :$PATH: != *:"~/bin":* ]] ; then
+if [[ :$PATH: != *:"$HOME/bin":* ]] ; then
    echo "adding ~/bin to PATH."
-   echo "PATH=\"\$PATH:~/bin\"" >> ~/.profile
+   echo "PATH=\"\$PATH:$HOME/bin\"" >> ~/.profile
 fi
 
